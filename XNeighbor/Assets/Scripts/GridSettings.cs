626b8945ee382015e3c9dfcfe67513185afbc4a0
Serdar Doğan <serdar.dogaan@gmail.com>
2022-08-13 16:14:50 +0300

    Last commit

--- XNeighbor/Assets/Scripts/GridSettings.cs
+++ XNeighbor/Assets/Scripts/GridSettings.cs
@@ -16,9 +16,19 @@ public class GridSettings : MonoBehaviour
 
     public void SetGridSettings(GridLayoutGroup gridLayoutGroup, int gridSize)
     {
-        float _areaAmountPerGridItem = screenSize.x / gridSize;
+        float _areaAmountPerGridItem = 0;
+        float gridItemSizeMultiplier = 0;
+        if (IsScreenPortrait())
+        {
+            _areaAmountPerGridItem = screenSize.x / gridSize;
+            gridItemSizeMultiplier = 0.9f;
+        } else
+        {
+            _areaAmountPerGridItem = screenSize.y / gridSize;
+            gridItemSizeMultiplier = 0.75f;
+        }
 
-        gridLayoutGroup.cellSize = new Vector2(_areaAmountPerGridItem * 0.9f / MainCanvas.transform.localScale.x, _areaAmountPerGridItem * 0.9f / MainCanvas.transform.localScale.y);
+        gridLayoutGroup.cellSize = new Vector2(_areaAmountPerGridItem * gridItemSizeMultiplier / MainCanvas.transform.localScale.x, _areaAmountPerGridItem * gridItemSizeMultiplier / MainCanvas.transform.localScale.y);
         gridLayoutGroup.spacing = new Vector2(_areaAmountPerGridItem * 0.05f / MainCanvas.transform.localScale.x, _areaAmountPerGridItem * 0.05f / MainCanvas.transform.localScale.y);
         gridLayoutGroup.padding = new RectOffset(
             (int)(_areaAmountPerGridItem * 0.05f),
@@ -30,4 +40,8 @@ public class GridSettings : MonoBehaviour
         gridLayoutGroup.constraintCount = gridSize;
     }
 
+    private bool IsScreenPortrait()
+    {
+        return screenSize.x <= screenSize.y;
+    }
 }
